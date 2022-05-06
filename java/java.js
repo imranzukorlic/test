54cccd2fe956d9e639dd16d34a1d32c30c4ce61f
Imran Zukorlic <mr.silver020@gmail.com>
2022-05-06 18:46:58 +0200

    js

---
+++ java/java.js
@@ -0,0 +1,20 @@
+alert("gasi")
+console.log("Prvi ispis u JS konzoli")
+'nesto'//string
+65 //number
+true,false //boolean
+['strng',62,true,['strng',62,true]]
+
+null //kad nema informacija
+undefined //kad podatatk postoji ali nije definisan
+{}//objekat
+
+var ime = 'ime je: isko'
+console.log(ime)
+ime = 132
+console.log(ime)
+
+console.log(7 % 3) //operacije +-*/%
+console.log('js'+'je legenda') //stringovi mogu samo d se sabiraju
+
+
