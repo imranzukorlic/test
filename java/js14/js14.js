a2ca2db7142859a983d34c5175460ee1abd2298b
Imran Zukorlic <mr.silver020@gmail.com>
2022-06-17 18:55:12 +0200

    asinhroni

---
+++ java/js14/js14.js
@@ -0,0 +1,19 @@
+// function nekiPosao() {
+//     console.log("pocetak posla");
+//     for (let i = 0; i < 90000000; i++) { }
+//     console.log("kraj posla");
+// }
+
+// nekiPosao()
+
+console.log("comand1");
+console.log("comand2");
+setTimeout(() => {
+    console.log("comand3");
+
+}, 1000);
+console.log("comand4");
+setTimeout(() => {
+    console.log("comand5");
+
+}, 999);
