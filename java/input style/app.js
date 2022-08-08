5b76b5380a75a725425fe5a298f75d2ad7fb3d93
Imran Zukorlic <mr.silver020@gmail.com>
2022-08-08 14:49:02 +0200

    n

--- java/input style/app.js
+++ java/input style/app.js
@@ -3,3 +3,13 @@ console.log(labels);
 labels.forEach((label)=>{
     label.innerHTML = label.innerText.split("").map((letter, idx)=>`<span style="transition-delay:${idx*50}ms">${letter}</span>`).join("")
 })
+// output.innerHTML = '<h2>Mause X '+e.offsetX +'</h2>'+'<h2>Mause Y'+e.offsetY+'</h2>'
+    // box.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+',40)'
+    // document.body.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+',40)'
+
+
+const color = document.querySelector('body')
+color.addEventListener('click', runEvent)
+function runEvent() {
+    document.body.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+',40)'
+}
